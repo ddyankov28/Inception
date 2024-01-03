3cb7016382483ac6746ace907c8e919a44b58c9c
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-01-03 20:34:21 +0100

    finish

--- srcs/wordpress/script_wordpress.sh
+++ srcs/wordpress/script_wordpress.sh
@@ -29,15 +29,27 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 						--role=author \
 						--allow-root
 
-	wp theme install "business-card-resume" --activate --allow-root
+	wp theme install "bravada" --activate --allow-root
 
-	wp post create --post_author=2 --post_name="first_post" --post_status=publish --post_content="<img src='https://unctad.org/sites/default/files/inline-images/about-evaluation_600x424.jpg' alt='HEY I JUST WANTED TO THANK YOU FOR EVALUATING ME' />" --post_title="WELCOME TO MY FIRST POST !" --allow-root
+	wp post delete 1 --force --allow-root
 	
-	wp post create --post_author=2 --post_name="second_post" --post_status=publish --post_content="" --post_title="THIS IS THE SECOND POST !" --allow-root
+	wp post delete 2 --force --allow-root
 	
-	wp media import /tmp/deyan.jpeg --post_id=4 --featured_image --allow-root
+	wp post create --post_author=2 --post_type=page --post_title='Car in the desert' --post_name="desert_car" --post_status=publish --post_content="<p>This is just an example page post with a car</p><img src='https://th.bing.com/th/id/OIG.JVG9Y3iI1cIhBjEFH91v?pid=ImgGn' />" --allow-root
 	
-	wp media import /tmp/softuni.jpeg --post_id=5 --featured_image --allow-root
+	wp post create --post_author=2 --post_type=page --post_title='Bulgarian stallion' --post_name="bg_stallion" --post_status=publish --post_content="This is example only with text" --allow-root
+	
+	wp post create --post_author=2 --post_name="first_post" --post_status=publish --post_content="<p>Hello, if you are reading this it means that you are my evaluator. Nice to see you here</p><img src='https://unctad.org/sites/default/files/inline-images/about-evaluation_600x424.jpg' alt='Alt Text' />" --post_title="MY FIRST POST" --allow-root
+	
+	wp post create --post_author=2 --post_name="second_post" --post_status=publish --post_content="<p>Hey there, i hope you like my wordpress</p><img src='https://image.spreadshirtmedia.net/image-server/v1/mp/products/T1459A839PA4459PT28D198581941W10000H6074/views/1,width=1200,height=630,appearanceId=839,backgroundColor=F2F2F2/42-zahl-nummer-sticker.jpg' alt='Alt Text' />" --post_title="THIS IS THE SECOND POST !" --allow-root
+	
+	wp media import /tmp/dd.jpeg --post_id=6 --featured_image --allow-root
+	
+	wp media import /tmp/dd1.jpeg --post_id=7 --featured_image --allow-root
+	
+	wp media import /tmp/impala.jpeg --post_id=4 --featured_image --allow-root
+	
+	wp media import /tmp/kon.jpeg --post_id=5 --featured_image --allow-root
 
 fi;
 
