2c31af3dadfd1c812a0f3b68e73cb8e1619f0375
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-01-03 16:46:58 +0100

    asd

--- srcs/wordpress/script_wordpress.sh
+++ srcs/wordpress/script_wordpress.sh
@@ -22,7 +22,6 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 						--admin_user=${WP_ADMIN} \
 						--admin_password=${WP_ADMIN_PASS} \
 						--admin_email=${WP_ADMIN_MAIL} \
-						--skip-email \
 						--allow-root
 
 	wp user create 		${WP_USER} ${WP_USER_MAIL} \
@@ -30,9 +29,15 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 						--role=author \
 						--allow-root
 
-	wp theme install "miyazaki" --activate --allow-root
+	wp theme install "business-card-resume" --activate --allow-root
 
-	wp post generate --count=1 --post_author="${WP_USER}" --post_title="WELCOME TO MY FIRST POST" --allow-root
+	wp post create --post_author=2 --post_name="first_post" --post_status=publish --post_content="<img src='https://unctad.org/sites/default/files/inline-images/about-evaluation_600x424.jpg' alt='HEY I JUST WANTED TO THANK YOU FOR EVALUATING ME' />" --post_title="WELCOME TO MY FIRST POST !" --allow-root
+	
+	wp post create --post_author=2 --post_name="second_post" --post_status=publish --post_content="" --post_title="THIS IS THE SECOND POST !" --allow-root
+	
+	wp media import /tmp/deyan.jpeg --post_id=4 --featured_image --allow-root
+	
+	wp media import /tmp/softuni.jpeg --post_id=5 --featured_image --allow-root
 
 fi;
 
